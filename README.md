437b4a944ba94963d94db7b3661a05bc7a703e4c
Ruslan Kurbanov <wkurbanov@gmail.com>
2017-01-02 01:28:42 +0300

    First Commit

--- README.md
+++ README.md
@@ -1,6 +1,11 @@
 # Night Owls Detector
 
-[TODO. There will be project description]
+The script gathers information from [devman.org API](http://devman.org/api/challenges/solution_attempts] about users that send his project to revision from 00:00 till 04:00. Then outputs username and timetable with his attempts.
+
+Usage:
+
+    pip install -r requirements.txt
+    python3.5 seek_dev_nighters.py
 
 # Project Goals
 
--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,15 +1,59 @@
-def load_attempts():
-    pages = 1
-    for page in range(pages):
-        # FIXME подключить загрузку данных из API
-        yield {
-            'username': 'bob',
-            'timestamp': 0,
-            'timezone': 'Europe/Moscow',
-        }
+import datetime
+import json
 
-def get_midnighters():
-    pass
+import pytz
+import requests
+
+URL = 'https://devman.org/api/challenges/solution_attempts'
+START_TIME = datetime.time(00, 00)
+END_TIME = datetime.time(4, 00)
+
+
+def get_number_of_pages():
+    request = requests.get(URL)
+    num_pages = json.loads(request.text)['number_of_pages']
+    return num_pages
+
+
+def load_attempts(num_pages):
+    for page in range(1, num_pages + 1):
+        request_params = {"page": page}
+        request = requests.get(URL, params=request_params)
+        records = json.loads(request.text)['records']
+        for record in records:
+            if record['timestamp']:
+                yield {
+                    'username': record['username'],
+                    'timestamp': float(record['timestamp']),
+                    'timezone': record['timezone'],
+                }
+
+
+def get_midnighters(users_data):
+    print("List of midnighters:")
+    midnighters = {}
+    for user_data in users_data:
+        username = user_data['username']
+        user_timezone = pytz.timezone(user_data['timezone'])
+        attempt_time = datetime.datetime.fromtimestamp(user_data['timestamp'], user_timezone)
+        if START_TIME <= attempt_time.time() <= END_TIME:
+            if username not in midnighters:
+                midnighters[username] = [attempt_time]
+            else:
+                midnighters[username].append(attempt_time)
+    return midnighters
+
+def print_midnighters(midnighters):
+    for username, attempts in midnighters.items():
+        attempts_formatted = [attempt.strftime("%H:%M:%S %d.%m.%y") for attempt in attempts]
+        print("Username: '{username}', Attempts:\n{attempts}\n".format(
+            username=username,
+            attempts="\n".join(attempts_formatted))
+        )
+
 
 if __name__ == '__main__':
-  pass
+    num_pages = get_number_of_pages()
+    users_data = load_attempts(num_pages)
+    midnighters = get_midnighters(users_data)
+    print_midnighters(midnighters)
